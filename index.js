a62548ab7c1d365bcdb6a88d833d895e20689959
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-18 19:31:21 +0000

    First Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function fixedAlert(){
+  const scrollNumber = window.scrollY;
+  const alertBar = document.querySelector('.alert');
+  if(scrollNumber > 50){
+    alertBar.classList.add('fixed');
+  }
+  else{
+    alertBar.classList.remove('fixed');
+  }
+}
