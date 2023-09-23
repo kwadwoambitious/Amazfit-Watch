e40b8bc92fca547354176139e3f7c4be3a690314
Antwi Ebenezer <antwiebenezer824@gmail.com>
2023-09-23 11:16:04 +0000

    Made changes to this file

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-
-
-function fixedAlert(){
-  const scrollNumber = window.scrollY;
-  const alertBar = document.querySelector('.alert');
-  if(scrollNumber > 50){
-    alertBar.classList.add('fixed');
-  }
-  else{
-    alertBar.classList.remove('fixed');
-  }
-}
+// function fixedAlert(){
+//   const scrollNumber = window.scrollY;
+//   const alertBar = document.querySelector('.alert');
+//   if(scrollNumber > 50){
+//     alertBar.classList.add('fixed');
+//   }
+//   else{
+//     alertBar.classList.remove('fixed');
+//   }
+// }
